d38c9c2dbbe169d272d872b588a3badbbf1d7857
felipe-Cruz-Savio <felipe.savio@escola.pr.gov.br>
2026-06-27 15:39:37 -0300

    .

---
+++ script.js
@@ -0,0 +1,20 @@
+const botao1 = document.getElementById('proxima1');
+if (botao1) {
+    botao1.addEventListener('click', function() {
+        window.location.href = 'dados.html';
+    });
+}
+
+const botao2 = document.getElementById('proxima2');
+if (botao2) {
+    botao2.addEventListener('click', function() {
+        window.location.href = 'aplicacoes.html';
+    });
+}
+
+const botao3 = document.getElementById('proxima3');
+if (botao3) {
+    botao3.addEventListener('click', function() {
+        window.location.href = 'index.html';
+    });
+}
